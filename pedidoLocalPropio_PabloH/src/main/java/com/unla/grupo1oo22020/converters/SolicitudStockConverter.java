2997da43641849c3dab9c352071ff6070a1be3ab
RomanoFelix16 <43682358+RomanoFelix16@users.noreply.github.com>
2020-06-03 12:19:07 -0300

    Update SolicitudStockConverter.java

--- pedidoLocalPropio_PabloH/src/main/java/com/unla/grupo1oo22020/converters/SolicitudStockConverter.java
+++ pedidoLocalPropio_PabloH/src/main/java/com/unla/grupo1oo22020/converters/SolicitudStockConverter.java
@@ -1,5 +1,32 @@
 package com.unla.grupo1oo22020.converters;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import com.unla.grupo1oo22020.entities.SolicitudStock;
+import com.unla.grupo1oo22020.models.SolicitudStockModel;
+
+@Component("solicitudStockConverter")
 public class SolicitudStockConverter {
+
+	@Autowired()
+	@Qualifier("productoConverter")
+	private ProductoConverter productoConverter;
+
+	@Autowired()
+	@Qualifier("localConverter")
+	private LocalConverter localConverter;
+
+	public SolicitudStockModel entityToModel(SolicitudStock solicitudStock) {
+		return new SolicitudStockModel(solicitudStock.getIdSolicitudStock(), solicitudStock.getFechaSolicitudStock(),
+				productoConverter.entityToModel(solicitudStock.getProducto()), solicitudStock.getCantidad(), solicitudStock.isAceptado(),
+				localConverter.entityToModel(solicitudStock.getLocal()));
+	}
+	
+	public SolicitudStock modelToEntity(SolicitudStockModel solicitudStockModel) {
+		return new SolicitudStock(solicitudStockModel.getIdSolicitudStock(), solicitudStockModel.getFechaSolicitudStock(),
+				productoConverter.modelToEntity(solicitudStockModel.getProducto()), solicitudStockModel.getCantidad(), localConverter.modelToEntity(solicitudStockModel.getLocal()));
+	}
 
 }
