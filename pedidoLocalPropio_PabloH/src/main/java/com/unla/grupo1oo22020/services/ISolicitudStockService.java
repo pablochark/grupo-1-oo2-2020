04bb26a004b249a9423c21d11ccf4c1b55ff6a75
RomanoFelix16 <43682358+RomanoFelix16@users.noreply.github.com>
2020-06-03 12:21:35 -0300

    Update ISolicitudStockService.java

--- pedidoLocalPropio_PabloH/src/main/java/com/unla/grupo1oo22020/services/ISolicitudStockService.java
+++ pedidoLocalPropio_PabloH/src/main/java/com/unla/grupo1oo22020/services/ISolicitudStockService.java
@@ -1,5 +1,22 @@
 package com.unla.grupo1oo22020.services;
 
+import com.unla.grupo1oo22020.models.SolicitudStockModel;
+
+import java.util.List;
+
+import com.unla.grupo1oo22020.entities.SolicitudStock;
+
 public interface ISolicitudStockService {
+	
+	// HACER INSERT, UPDATE, GETALL, REMOVE
+
+	public List<SolicitudStock> getAll();
+	
+	public SolicitudStockModel insert(SolicitudStockModel solicitudStockModel);
+	
+	public SolicitudStockModel update(SolicitudStockModel solicitudStockModel);
+	
+	public boolean remove(long idSolicitudStock);
+	
 
 }
