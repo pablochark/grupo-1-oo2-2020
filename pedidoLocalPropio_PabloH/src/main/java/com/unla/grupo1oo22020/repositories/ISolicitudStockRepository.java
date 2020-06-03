03946a0051bbde153d561c2368bf8718ed6374c8
RomanoFelix16 <43682358+RomanoFelix16@users.noreply.github.com>
2020-06-03 12:21:00 -0300

    Update ISolicitudStockRepository.java

--- pedidoLocalPropio_PabloH/src/main/java/com/unla/grupo1oo22020/repositories/ISolicitudStockRepository.java
+++ pedidoLocalPropio_PabloH/src/main/java/com/unla/grupo1oo22020/repositories/ISolicitudStockRepository.java
@@ -1,5 +1,13 @@
 package com.unla.grupo1oo22020.repositories;
 
-public interface ISolicitudStockRepository {
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.io.Serializable;
 
+import com.unla.grupo1oo22020.entities.SolicitudStock;
+
+@Repository("solicitudStockRepository")
+public interface ISolicitudStockRepository extends JpaRepository<SolicitudStock, Serializable> {
+	
+	public abstract SolicitudStock findByIdSolicitudStock(long idSolicitudStock);
 }
